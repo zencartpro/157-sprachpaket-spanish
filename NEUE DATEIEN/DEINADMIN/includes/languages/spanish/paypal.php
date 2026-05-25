dd51df30c964bcd1de66f3722bdbf24e357ef15c
Jörg Schönberger <office@webchills.at>
2026-05-25 13:24:07 +0200

    Version 1.4.2

--- NEUE DATEIEN/DEINADMIN/includes/languages/spanish/paypal.php
+++ NEUE DATEIEN/DEINADMIN/includes/languages/spanish/paypal.php
@@ -1,13 +1,12 @@
 <?php
 /**
- * Zen Cart German Specific (158 code in 157)
-* @copyright Copyright 2003-2023 Zen Cart Development Team
- * Zen Cart German Version - www.zen-cart-pro.at
+* Zen Cart German Specific (158 code in 157)
+* @copyright Copyright 2003-2026 Zen Cart Development Team
+* Zen Cart German Version - www.zen-cart-pro.at
 * @copyright Portions Copyright 2003 osCommerce
 * @license https://www.zen-cart-pro.at/license/3_0.txt GNU General Public License V3.0
-* @version $Id: paypal.php 2023-10-28 19:49:16Z webchills $
- */
-
+* @version $Id: paypal.php 2026-04-04 11:49:16Z webchills $
+*/
 
 define('TEXT_PAYPAL_IPN_SORT_ORDER_INFO', 'Anzeigesortierung: ');
 define('TEXT_SORT_PAYPAL_ID_DESC', 'PayPal Sortierung (neu - alt)');
@@ -17,8 +16,7 @@ define('TEXT_SORT_ZEN_ORDER_ID', 'Order ID (niedrig - hoch), PayPal Sortierung')
 define('TEXT_PAYMENT_AMOUNT_DESC', 'Bestellsumme (hoch - niedrig)');
 define('TEXT_PAYMENT_AMOUNT', 'Bestellsumme (niedrig - hoch)');
 
-
-define('HEADING_ADMIN_TITLE', 'PayPal sofortige Zahlungsbenachrichtigungen');
+define('HEADING_ADMIN_TITLE', 'PayPal Transaktionen');
 define('HEADING_PAYMENT_STATUS', 'Bezahlstatus');
 define('TEXT_ALL_IPNS', 'Alle');
 define('TABLE_HEADING_ORDER_NUMBER', 'Bestellnummer');
@@ -33,5 +31,3 @@ define('TABLE_HEADING_TRANS_ID', 'Trans. ID');
 define('TABLE_HEADING_PENDING_REASON', 'Pending Grund');
 define('TEXT_INFO_PAYPAL_IPN_HEADING', 'PayPal IPN');
 define('TEXT_DISPLAY_PAYPAL_IPN_NUMBER_OF_TX', 'Zeige <strong>%d</strong> bis <strong>%d</strong> (von <strong>%d</strong> Transaktionen)');
-
-
